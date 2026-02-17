551fc7c48295dab8a6a395b38d2d76ad62dc9c21
harinivvenkat28 <harini.vvenkat@gmail.com>
2026-02-17 10:44:13 +0530

    Add GPIO control for number matrix display
    Initialize GPIO pins and control outputs based on a number matrix.

--- mylist.py
+++ mylist.py
@@ -1,1 +1,16 @@
-#Create any list
+from machine import Pin
+import time
+
+a=Pin(33,Pin.OUT)
+b=Pin(5,Pin.OUT)
+c=Pin(9,Pin.OUT)
+d=Pin(18,Pin.OUT)
+
+my_num = [[1,0,0,0],[0,1,0,0][0,0,1,0][0,0,0,1]]
+
+for i in my_num:
+    a.value(i[0])
+    b.value(i[1])
+    c.value(i[2])
+    d.value(i[3])
+    time.sleep(1)
